5da138a826100d325f432c3c7aefbd216a3e1d6a
CodeDraken <codedraken@users.noreply.github.com>
2018-09-08 14:11:20 -0500

    Preload Electron into React

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,4 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { BrowserWindow, ipcMain, Menu, app, ipcRenderer } = require('electron')
 
 const path = require('path')
 const url = require('url')
@@ -9,7 +7,13 @@ const isDev = require('electron-is-dev')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({ width: 800, height: 680, webPreferences: { webSecurity: !isDev } })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 680,
+    webPreferences: {
+      webSecurity: !isDev,
+      preload: path.join(__dirname, 'electron-main', 'preload.js')
+    } })
 
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
 
@@ -31,3 +35,8 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+// ipcMain.on('mount-app', (event, arg) => {
+//   console.log('mount app main')
+//   mainWindow.webContents.send('okay', 'hello')
+// })
--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  state = {
+    message: ''
+  }
+
+  componentDidMount () {
+    // const { ipcRenderer } = window.electron
+    // ipcRenderer.send('mount-app')
+
+    // ipcRenderer.on('okay', (event, arg) => {
+    //   this.setState({
+    //     message: arg
+    //   })
+    // })
+  }
+
   render () {
     return (
       <div className='App'>
@@ -11,6 +26,7 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.message}
       </div>
     )
   }
