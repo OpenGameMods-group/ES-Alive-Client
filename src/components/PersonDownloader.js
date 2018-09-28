3d8027b69c4418395391b20f858e898edfa0c582
CodeDraken <codedraken@users.noreply.github.com>
2018-09-27 21:15:05 -0500

    Add levellimit input

--- src/components/PersonDownloader.js
+++ src/components/PersonDownloader.js
@@ -10,14 +10,67 @@ const defaultProps = {
   children: null
 }
 
-export const PersonDownloader = ({ children, ...props }) => {
-  const { ...attributes } = props
+class PersonDownloader extends React.Component {
+  state = {
+    levelLimit: 10
+  }
 
-  return (
-    <div>
-      PersonDownloader
-    </div>
-  )
+  componentDidMount () {
+    const levelLimit = window.localStorage.getItem('levelLimit')
+    if (levelLimit) {
+      this.setState({
+        levelLimit
+      })
+    }
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      levelLimit: evt.target.value
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    const { levelLimit } = this.state
+
+    window.localStorage.setItem('levelLimit', levelLimit)
+  }
+
+  render () {
+    const { children, ...props } = this.props
+
+    return (
+      <div>
+        <form className='columns'>
+          <div className='column col-8'>
+
+            <div className='form-group'>
+              <label className='form-label'>
+              Fleet Level Limit
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.levelLimit}
+                  name='levelLimit'
+                  type='number'
+                  placeholder='levelLimit'
+                  className='form-input'
+                  required
+                />
+              </label>
+            </div>
+
+            <div>
+              <button className='btn' onClick={this.handleSubmit}>
+                Download Pilots
+              </button>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 PersonDownloader.propTypes = propTypes
